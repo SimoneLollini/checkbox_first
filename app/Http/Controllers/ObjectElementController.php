edc1249680f7bfee246e2d794997bd2da771f9e4
SimoneLollini <lollinisimone@gmail.com>
2022-12-25 00:59:41 +0100

    first commit

---
+++ app/Http/Controllers/ObjectElementController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreObjectElementRequest;
+use App\Http\Requests\UpdateObjectElementRequest;
+use App\Models\ObjectElement;
+
+class ObjectElementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreObjectElementRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreObjectElementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ObjectElement  $objectElement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ObjectElement $objectElement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ObjectElement  $objectElement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ObjectElement $objectElement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateObjectElementRequest  $request
+     * @param  \App\Models\ObjectElement  $objectElement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateObjectElementRequest $request, ObjectElement $objectElement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ObjectElement  $objectElement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ObjectElement $objectElement)
+    {
+        //
+    }
+}
---
+++ database/seeders/ObjectElementSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ObjectElement;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use PhpParser\Node\Expr\Cast\Object_;
+
+class ObjectElementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newObjectEl = new ObjectElement();
+            $newObjectEl->title = $faker->word;
+            $newObjectEl->customer = $faker->name;
+            $newObjectEl->done = $faker->boolean;;
+            $newObjectEl->save();
+        }
+    }
+}
